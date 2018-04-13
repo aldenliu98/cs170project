8939d0120140f0c4104b17408a11c33f29a02ed6
Kelvin Zhang <kelvin1998@gmail.com>
2018-04-13 01:02:51 -0700

    graph

--- graphgenerator.py
+++ graphgenerator.py
@@ -24,32 +24,59 @@ G=fast_gnp_random_graph(n, 0.4)
 
 A = nx.adjacency_matrix(G)
 
-
 for u,v,d in G.edges(data=True):
 	d['weight'] = random.randint(1,11)
 
 shortest = dict(nx.floyd_warshall(G))
+
+adj_m = [[0 for x in range(n)] for y in range(n)]
+
 
 for u, v, datadict in G.edges(data=True):
 	if datadict['weight'] > shortest[u][v]:
 		datadict['weight'] = shortest[u][v]
 
 
+	adj_m[u][v] = datadict['weight']
+	adj_m[v][u] = datadict['weight']
 
 
-def create_file(A):
-	with open('TESTER.txt','w') as f:
-		for line in A:
-			np.savetxt(f, line, fmt='%.2f')
+for i in range(len(adj_m)):
+	for j in range(len(adj_m[0])):
+		if(i==j):
+			adj_m[i][j] = random.randint(1,11)
+		
+		if(adj_m[i][j] == 0):
+			adj_m[i][j] = 'x'
+
+
+
+
+def create_file(n, adj_list):
+	f = open("50.in", "w")
+	f.write(str(n)+"\n")
+
+	for i in range(n):
+		f.write(str(i))
+		if(i != n-1):
+			f.write(" ")
+
+	f.write("\n")
+	f.write(str(0) + "\n")
+	for i in range(len(adj_list)):
+		for j in range(len(adj_list[0])):
+			f.write(str(adj_list[i][j]))
+			if(j != len(adj_list[0]) - 1):
+				f.write(" ")
+		if(i != len(adj_list) - 1):
+			f.write("\n")
+
 	f.close()
 
+create_file(n, adj_m)
 
-<<<<<<< HEAD
+
+
 nx.draw(G)
 nx.write_gexf(G, "./graphs/textWeights.gexf")
 
-=======
-
-nx.draw(G)
-nx.write_gexf(G, "./graphs/50node.gexf")
->>>>>>> 1069a5e0f902b60e17c17b6afd2819ea9eb0dded
