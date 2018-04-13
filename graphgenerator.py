828ca7bc4c015ed0ebecd57e990b40f67754a6f2
Kelvin Zhang <kelvin1998@gmail.com>
2018-04-12 22:25:17 -0700

    graphs

---
+++ graphgenerator.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import scipy as sp
+from networkx import *
+import sys
+
+n= 50
+e = 580
+
+G=fast_gnp_random_graph(n, 0.4)
+# some properties
+print("node degree clustering")
+for v in nodes(G):
+    print('%s %d %f' % (v,degree(G,v),clustering(G,v)))
+
+# print the adjacency list to terminal
+try:
+    write_adjlist(G,sys.stdout)
+except TypeError: # Python 3.x
+    write_adjlist(G,sys.stdout.buffer)
+
+A = nx.to_numpy_matrix(G)
+
+for i in range(len(A)):
+	print(A[i])
+print(A)
+
+nx.draw(G)
+plt.draw()
